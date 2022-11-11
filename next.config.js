243a80b38d358f36f8a4dae3752104561fe251ee
Gezart <gezart.dervishi@gmail.com>
2022-11-11 16:27:06 +0100

    images

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-// module.exports = {
-//     images: {
-//       remotePatterns: [
-//         {
-//           protocol: 'http',
-//           hostname: '13.38.32.150',
-//           port: '',
-//           pathname: '/wp-content/uploads/',
-//         },
-//       ],
-//     },
-//   }
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: '13.38.32.150',
+          port: '',
+          pathname: '/wp-content/uploads/',
+        },
+      ],
+    },
+  }
