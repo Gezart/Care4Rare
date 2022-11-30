9d43dbda138096f90ccb059e67754c0ea6759334
Gezart <gezart.dervishi@gmail.com>
2022-11-30 08:47:11 +0100

    testing home

--- pages/index.js
+++ pages/index.js
@@ -1,148 +1,33 @@
 import { gql } from '@apollo/client'
 import Head from 'next/head'
 import React from 'react'
-import Banner from '../components/Banner'
-import GoToJobs from '../components/GoToJobs'
 import Layout from '../components/Layout'
-import Partners from '../components/Partners'
-import PlainText from '../components/PlainText'
-import Services from '../components/Services'
-import Text from '../components/Text'
 import { client } from '../lib/apollo'
 
-const index = ({ homeData, contactData, menu }) => {
-    let sections = homeData.sections.sections
+const index = ({contactData, menu }) => {
     let mainMenu = menu?.edges[0]?.node?.menuItems?.nodes;
     let rightMenu = menu?.edges[1]?.node?.menuItems?.nodes;
     return (
         <>
-            <Head>
-                <title>Care4Rare</title>
-                <link rel="icon" href="favicon.ico"></link>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet"></link> */}
-            </Head>
-            <Layout contactData={contactData} mainMenu={mainMenu} rightMenu={rightMenu}>
-                <main className='page page-home'>
-                    {
-                        sections?.map((section, index) => {
-                            const typeName = section.__typename;
-                            switch (typeName) {
-                                case 'Page_Sections_Sections_Banner':
-                                    return <Banner {...section} key={index} />
-                                case 'Page_Sections_Sections_Text':
-                                    return <Text {...section} key={index} />
-                                case 'Page_Sections_Sections_PlainText':
-                                    return <PlainText {...section} key={index} />
-                                case 'Page_Sections_Sections_Services':
-                                    return <Services {...section} contactData={contactData} key={index} />
-                                case 'Page_Sections_Sections_Partners':
-                                    return <Partners {...section} key={index} />
-                                case 'Page_Sections_Sections_GoToJobs':
-                                    return <GoToJobs {...section} key={index} />
+        <Head>
+            <title>Care4Rare</title>
+            <link rel="icon" href="favicon.ico"></link>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet"></link> */}
+        </Head>
+        <Layout contactData ={contactData} mainMenu={mainMenu} rightMenu={rightMenu}>
 
-                                default:
-                                    return ''
-
-                            }
-                        })
-                    }
-                </main>
-            </Layout>
-        </>
-    )
+        </Layout>
+    </>
+  )
 }
 
 export default index
 
-export async function getStaticProps() {
+export async function getStaticProps(){
     const homeQuery = gql`
     query homeQuery {
-        pages(where: {title: "Home"}) {
-            nodes {
-              title
-              slug
-              uri
-              sections {
-                sections {
-                  ... on Page_Sections_Sections_Banner {
-                    bannerSize
-                    title
-                    content
-                    image {
-                      mediaItemUrl
-                      mediaDetails {
-                        file
-                      }
-                    }
-                    bannerServices {
-                      icon
-                      title
-                      content
-                    }
-                  }
-                  ... on Page_Sections_Sections_Text {
-                    title
-                    content
-                  }
-                  ... on Page_Sections_Sections_PlainText {
-                    content
-                    button {
-                      title
-                      url
-                    }
-                    images {
-                      mediaDetails {
-                        file
-                      }
-                    }
-                  }
-                  ... on Page_Sections_Sections_Services {
-                    services {
-                      title
-                      subservices {
-                        title
-                        content
-                        image {
-                          mediaItemUrl
-                          mediaDetails {
-                            file
-                          }
-                        }
-                      }
-                    }
-                  }
-                  ... on Page_Sections_Sections_Partners {
-                    partnersTitle
-                    backgroundImage {
-                      mediaItemUrl
-                    }
-                    partner {
-                      link {
-                        url
-                      }
-                      image {
-                        mediaItemUrl
-                      }
-                    }
-                  }
-                  ... on Page_Sections_Sections_GoToJobs {
-                    goToJobs {
-                      title
-                      link {
-                        title
-                        url
-                      }
-                      image {
-                        mediaItemUrl
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
         acfOptionsThemeOption {
           themeOptions {
             logo
@@ -208,16 +93,14 @@ export async function getStaticProps() {
 
     const response = await client.query({
         query: homeQuery
-    })
-    const homeData = response?.data?.pages?.nodes[0]
-    const contactData = response?.data?.acfOptionsThemeOption?.themeOptions
-    const menu = response?.data?.menus
-    return {
+      })
+      const contactData = response?.data?.acfOptionsThemeOption?.themeOptions
+      const menu = response?.data?.menus
+      return {
         props: {
-            homeData,
-            contactData,
-            menu
+          contactData, 
+          menu
         }
-    }
+      }
 
 }
