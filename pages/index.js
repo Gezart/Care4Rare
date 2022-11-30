16527979ada71297d8f8db20ac22814d4a78ac80
Gezart <gezart.dervishi@gmail.com>
2022-11-30 08:50:56 +0100

    testing home

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
-import { gql } from '@apollo/client'
-import Head from 'next/head'
 import React from 'react'
-import Layout from '../components/Layout'
-import { client } from '../lib/apollo'
 
-const index = ({contactData, menu }) => {
-    let mainMenu = menu?.edges[0]?.node?.menuItems?.nodes;
-    let rightMenu = menu?.edges[1]?.node?.menuItems?.nodes;
+const index = () => {
     return (
         <>
         <Head>
@@ -16,91 +10,8 @@ const index = ({contactData, menu }) => {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet"></link> */}
         </Head>
-        <Layout contactData ={contactData} mainMenu={mainMenu} rightMenu={rightMenu}>
-
-        </Layout>
     </>
   )
 }
 
 export default index
-
-export async function getStaticProps(){
-    const homeQuery = gql`
-    query homeQuery {
-        acfOptionsThemeOption {
-          themeOptions {
-            logo
-            footerDescription
-            footerMenuTitle
-            footerMenu {
-              title {
-                ... on Page {
-                  title
-                  uri
-                }
-              }
-            }
-            policyTitle
-            policyMenu {
-              title {
-                ... on Page {
-                  title
-                  uri
-                }
-              }
-            }
-            locationIcon
-            locationTitle
-            location
-            location2
-            emailIcon
-            emailTitle
-            email {
-              title
-              url
-            }
-            phoneIcon
-            phoneTitle
-            phone {
-              title
-              url
-            }
-            mobileMenu {
-              icon
-              page {
-                url
-                title
-              }
-            }
-          }
-        }
-        menus {
-          edges {
-            node {
-              slug
-              menuItems {
-                nodes {
-                  uri
-                  label
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-
-    const response = await client.query({
-        query: homeQuery
-      })
-      const contactData = response?.data?.acfOptionsThemeOption?.themeOptions
-      const menu = response?.data?.menus
-      return {
-        props: {
-          contactData, 
-          menu
-        }
-      }
-
-}
