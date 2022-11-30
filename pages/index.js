8c7ff864fbd1bae6aaed2847951ad6335c171d87
Gezart <gezart.dervishi@gmail.com>
2022-11-30 08:52:06 +0100

    testing home

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,6 @@ const index = () => {
         <Head>
             <title>Care4Rare</title>
             <link rel="icon" href="favicon.ico"></link>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-            {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet"></link> */}
         </Head>
     </>
   )
